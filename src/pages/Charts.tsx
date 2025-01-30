b6ef47549c1e2acfe9d8d619ac92a6fe22958102
lazalajochy <lazala1401jochy@outlook.com>
2025-01-29 22:38:59 -0400

    new name

--- src/pages/Charts.tsx
+++ src/pages/Charts.tsx
@@ -1,9 +1,10 @@
-import {BsFillGrid3X3GapFill} from 'react-icons/bs'
+import { BsFillGrid3X3GapFill } from 'react-icons/bs'
 import { GiPayMoney } from "react-icons/gi";
 import { MdPayments, MdPayment } from "react-icons/md";
 import apiClient from '../utils/request';
 import { useEffect, useState } from 'react';
-
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line }
+    from 'recharts';
 interface ChartsProps {
     openModal: (content: string) => void;
 }
@@ -25,27 +26,36 @@ interface Category {
 };
 
 
-function Charts({}: ChartsProps) {
+function Charts({ }: ChartsProps) {
     const [currentIncome, setCurrentIncome] = useState<Income[]>([]);
     const [currentCategory, setCurrentCategory] = useState<Category[]>([]);
     const [totalPayment, setTotalPayment] = useState(0);
     const [reminder, setReminder] = useState(0);
+    const [data, setData] = useState<{ name: string; uv: number }[]>([]);
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
     const getIncomes = async () => {
         const token = localStorage.getItem("token");
-        const response  = await apiClient<{msg: Income[]}>("/income", {}, "GET", token as string);
+        const response = await apiClient<{ msg: Income[] }>("/income", {}, "GET", token as string);
         const incomes = response.msg;
         setCurrentIncome(incomes);
     };
 
     const getCategories = async () => {
         const token = localStorage.getItem("token");
-        const response = await apiClient<{msg: Category[]}>("/category", {}, "GET", token as string);
+        const response = await apiClient<{ msg: Category[] }>("/category", {}, "GET", token as string);
         const categories = response.msg;
+        const categoryData = categories.map(category => ({
+            name: category?.name ?? "",
+            uv: category?.amount ?? 0
+        }))
+        setData(categoryData)
         const payment = categories.reduce((acc, category) => acc + category.amount, 0);
         setTotalPayment(payment);
         setCurrentCategory(categories);
     };
+
+
 
     useEffect(() => {
         getIncomes();
@@ -53,8 +63,10 @@ function Charts({}: ChartsProps) {
     }, []);
 
     useEffect(() => {
-         (typeof currentIncome[0]?.salary === 'number' && typeof totalPayment === 'number') ? setReminder(currentIncome[0].salary - totalPayment): setReminder(0); 
-    },[currentIncome, currentCategory]);
+        (typeof currentIncome[0]?.salary === 'number' && typeof totalPayment === 'number') ? setReminder(currentIncome[0].salary - totalPayment) : setReminder(0);
+        console.log(data, "this is  the end data")
+    }, [currentIncome, currentCategory, data]);
+
 
     return (
         <main className="main-container">
@@ -93,6 +105,32 @@ function Charts({}: ChartsProps) {
                     </div>
                     <h1>RD$ {totalPayment ?? 0}</h1>
                 </div>
+            </div>
+            <div className='charts'>
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5
+                        }}
+                        style={{ background: '#ffffff' }} 
+
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Legend />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="uv" fill="#82ca9d" />
+                    </BarChart>
+                </ResponsiveContainer>
+
             </div>
         </main>
     );
